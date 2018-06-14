26825e4a43bca4a8968e0470458b7e65ca706697
Francisco Carmona <fcarmona@specialtours.com>
2018-06-14 13:24:11 +0200

    feat: add main component

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Main></Main>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 import Footer from './components/Footer.vue'
 
 export default {
   name: 'app',
   components: {
     Header,
+    Main,
     Footer
   }
 }
---
+++ src/app/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    Main
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
