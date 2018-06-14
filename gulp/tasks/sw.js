455df8115e04bf5a044ca38734ad55ebc43efbbe
Francisco Carmona <fcarmona@specialtours.com>
2018-06-14 17:03:01 +0200

    feat (wip): add service worker

--- gulp/tasks/sw.js
+++ gulp/tasks/sw.js
@@ -2,12 +2,16 @@ let gulp = require('gulp'),
     browserifyBuild = require('./browserifyES2015');
 
 module.exports = (paths, plugins, watch, browserSync) => {
-  return () => browserifyBuild({
-    src: paths.src,
-    watch: watch,
-    dstPath: paths.dst,
-    dstFile: 'sw.js',
-    browserify: plugins.browserify,
-    browserSync: browserSync
-  });
+
+  return () => gulp.src(paths.src)
+    .pipe(gulp.dest(paths.dst));
+
+  // return () => browserifyBuild({
+  //   src: paths.src,
+  //   watch: watch,
+  //   dstPath: paths.dst,
+  //   dstFile: 'sw.js',
+  //   browserify: plugins.browserify,
+  //   browserSync: browserSync
+  // });
 };
--- src/app/index.js
+++ src/app/index.js
@@ -5,3 +5,12 @@ new Vue({
   el: '#app',
   render: h => h(App)
 });
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js').then(() => {
+    console.log('Service Worker registered');
+  })
+    .catch((error) => {
+      console.log('Registration failed with ' + error);
+    });
+}
